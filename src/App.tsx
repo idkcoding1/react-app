57d88e2a25cc00d07a1b7ae6dea9631adba23d96
Tanveer Hamza <hamzatanveer961@gmail.com>
2024-09-06 12:30:57 +0500

    check

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ function App() {
   return (
     <>
     <h1 className='mb-4'>
-      Heloo world
+    Hello World 
     </h1>
-    <p>My name is tanveer hamza</p>
+    <p>My name is tanveer hamza
+      A web Developer Enthausiast 
+    </p>
     </>
   )
 }
